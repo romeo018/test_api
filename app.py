e9b5493e15b197d953bcccbbc221e2f835c1f661
romeo018 <pirates4@yandex.ru>
2025-08-27 22:58:46 +0500

    Дальше

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"ты ввел = ": item_id, "q": q}
+
+
+@app.get("/add/{a}/{b}")
+def add(a: float, b: float):
+    return {"result": a + b}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
